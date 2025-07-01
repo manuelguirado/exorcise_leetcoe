a7d5ed895088926f2bf2231e3c2b3731df5524c1
manuelguirado <manu22.mgb@gmail.com>
2025-07-01 17:43:40 +0200

    add: adding removeDuplicates funtionality

---
+++ removeDuplicates.ts
@@ -0,0 +1,12 @@
+function removeDuplicates(nums: number[]): number {
+    if (nums.length === 0) return 0;
+
+    let uniqueIndex = 0;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[uniqueIndex]) {
+            uniqueIndex++;
+            nums[uniqueIndex] = nums[i];
+        }
+    }
+    return uniqueIndex + 1;
+};
