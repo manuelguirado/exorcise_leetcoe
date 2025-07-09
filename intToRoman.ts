553b5ae184c32dc650c17db97e53a90bdeb4f791
manuelguirado <manu22.mgb@gmail.com>
2025-07-09 18:41:16 +0200

    add: implement integer to Roman numeral conversion function

---
+++ intToRoman.ts
@@ -0,0 +1,27 @@
+function intToRoman(num: number): string {
+    const romanNumerals: [number, string][] = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ];
+    let result = '';
+    for (const [value, numeral] of romanNumerals) {
+        while (num >= value) {
+            result += numeral;
+            num -= value;
+        }
+    }
+    return result;
+};
+let num = 1994; // Example input
+console.log(intToRoman(num)); // Output: "MCMXCIV"
